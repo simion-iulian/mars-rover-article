5121333e8e51d10d68278754f7205ea9cb5589f4
simion-iulian <simion.belea@codurance.com>
2018-11-09 17:18:55 +0000

    Extracted format to field

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,13 +1,19 @@
 public class MarsRover {
   private final String NORTH = "N";
   private final String SOUTH = "S";
+
+  private final int UP = 1;
+  private final int DOWN = -1;
+
+  private final String COORDINATE_FORMAT = "%d %d %s";
+
+  private String MOVE_COMMAND = "M";
+
   private int x;
   private int y;
   private String cardinal;
-  private String MOVE_COMMAND = "M";
 
   public MarsRover(int x, int y, String cardinal) {
-
     this.x = x;
     this.y = y;
     this.cardinal = cardinal;
@@ -26,9 +32,13 @@ public class MarsRover {
 
   private void move() {
     if(facing(NORTH))
-      y++;
+      moveVertically(UP);
     if(facing(SOUTH))
-      y--;
+      moveVertically(DOWN);
+  }
+
+  private void moveVertically(int stepSize) {
+    y += stepSize;
   }
 
   private boolean isMove(String command) {
@@ -40,6 +50,6 @@ public class MarsRover {
   }
 
   private String formatCoordinate() {
-    return String.format("%d %d %s", x, y, cardinal);
+    return String.format(COORDINATE_FORMAT, x, y, cardinal);
   }
 }
