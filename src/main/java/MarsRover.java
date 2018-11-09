f8cb14d3f71c7784af82ae45bdbef2a9a9807dc8
simion-iulian <simion.belea@codurance.com>
2018-11-09 16:16:38 +0000

    Refactored coordinate to use String format

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,9 +1,20 @@
 public class MarsRover {
+  private final int x;
+  private final int y;
+  private final String cardinal;
+
   public MarsRover(int x, int y, String cardinal) {
 
+    this.x = x;
+    this.y = y;
+    this.cardinal = cardinal;
   }
 
   public String execute(String commands) {
-    return "1 2 N";
+    return formatCoordinate();
+  }
+
+  private String formatCoordinate() {
+    return String.format("%d %d %s", x, y, cardinal);
   }
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -7,10 +7,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class MarsRoverShould {
 
   @ParameterizedTest
-  @CsvSource({"1, 2, N, '1 2 N'"})
-  void return_initial_position_of_rover_without_any_command(int initialX, int initialY, String initialCardinal, String expectedCoordinate) {
-    final String emptyCommand = "";
+  @CsvSource({
+    "1, 2, N, '1 2 N'",
+    "1, 3, N, '1 3 N'"
+  })
+  void return_initial_position_of_rover_without_any_command(
+    int initialX, int initialY, String initialCardinal,
+    String expectedCoordinate) {
 
-    assertThat(new MarsRover(initialX, initialY, initialCardinal).execute(emptyCommand), CoreMatchers.is(expectedCoordinate));
+    final String emptyCommand = "";
+    final MarsRover rover = new MarsRover(initialX, initialY, initialCardinal);
+
+    final String actualCoordinate = rover.execute(emptyCommand);
+
+    assertThat(actualCoordinate, CoreMatchers.is(expectedCoordinate));
   }
 }
