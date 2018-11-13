dd5759ac6f511410331efbfda0be550e0a53d65c
simion-iulian <simion.belea@codurance.com>
2018-11-13 16:30:20 +0000

    Changed tests to clarify use of Position object

--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -16,7 +16,8 @@ public class MarsRoverShould {
     String expectedCoordinate) {
 
     final String emptyCommand = "";
-    final MarsRover rover = new MarsRover(new Position(initialX, initialY, initialCardinal));
+    final Position initialPosition = new Position(initialX, initialY, initialCardinal);
+    final MarsRover rover = new MarsRover(initialPosition);
 
     final String actualCoordinate = rover.execute(emptyCommand);
 
@@ -42,7 +43,8 @@ public class MarsRoverShould {
     String commands,
     String expectedCoordinate
   ) {
-    final MarsRover rover = new MarsRover(new Position(initialX, initialY, initialCardinal));
+    final Position initialPosition = new Position(initialX, initialY, initialCardinal);
+    final MarsRover rover = new MarsRover(initialPosition);
 
     String actualCoordinate = rover.execute(commands);
 
