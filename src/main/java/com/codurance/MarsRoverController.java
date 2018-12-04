8f6c89a5044c0fdbb0c568018e8af736e01ed38c
simion-iulian <simion.belea@codurance.com>
2018-12-04 17:33:46 +0000

    Cleaned up MarsRoverController

--- src/main/java/com/codurance/MarsRoverController.java
+++ src/main/java/com/codurance/MarsRoverController.java
@@ -17,18 +17,6 @@ public class MarsRoverController {
     return rover.formatPosition();
   }
 
-  private boolean isTurnLeft(String command) {
-    String LEFT_COMMAND = "L";
-    return command.equals(LEFT_COMMAND);
-  }
-  private boolean isTurnRight(String command) {
-    String RIGHT_COMMAND = "R";
-    return command.equals(RIGHT_COMMAND);
-  }
-  private boolean isMove(String command) {
-    String MOVE_COMMAND = "M";
-    return command.equals(MOVE_COMMAND);
-  }
   private String[] commandsFrom(String input) {
     String INTO_CHARACTERS = "";
     return input.split(INTO_CHARACTERS);
